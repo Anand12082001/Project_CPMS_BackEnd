7d0bc16c80c44e7aaaa1e8497f9100e07f63d1ad
Anand12082001 <Anandjayakumardae@gmail.com>
2026-01-03 16:02:30 +0530

    Fix email sending using Gmail OAuth2 and clean env handling

--- config/Nodemailer.js
+++ config/Nodemailer.js
@@ -1,74 +1,28 @@
-// // import nodemailer from "nodemailer";
-// const nodemailer = require("nodemailer");
-// require("dotenv").config();
-
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS,
-//   }
-// });
-
-// const sendMail = async (to, subject, html) => {
-//   const mailOptions = {
-//     from: `"CPMS" ${process.env.EMAIL_USER}`,
-//     to,
-//     subject,
-//     html
-//   };
-
-//   try {
-//     const info = await transporter.sendMail(mailOptions);
-//     return info;
-//   } catch (error) {
-//     console.log("Error sending email: ", error);
-//   }
-// };
-
-// module.exports = sendMail;
+// import nodemailer from "nodemailer";
 const nodemailer = require("nodemailer");
-const { google } = require("googleapis");
+require("dotenv").config();
 
-const OAuth2 = google.auth.OAuth2;
-
-const oauth2Client = new OAuth2(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
-  "https://developers.google.com/oauthplayground"
-);
-
-oauth2Client.setCredentials({
-  refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  }
 });
 
 const sendMail = async (to, subject, html) => {
+  const mailOptions = {
+    from: `"CPMS" ${process.env.EMAIL_USER}`,
+    to,
+    subject,
+    html
+  };
+
   try {
-    const accessToken = await oauth2Client.getAccessToken();
-
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        type: "OAuth2",
-        user: process.env.EMAIL_USER,
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-        accessToken: accessToken.token,
-      },
-    });
-
-    const info = await transporter.sendMail({
-      from: `"CPMS" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      html,
-    });
-
-    console.log("✅ Gmail API email sent:", info.response);
+    const info = await transporter.sendMail(mailOptions);
+    return info;
   } catch (error) {
-    console.error("❌ Gmail API error:", error);
-    throw error;
+    console.log("Error sending email: ", error);
   }
 };
 
