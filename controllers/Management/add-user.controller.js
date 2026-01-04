38969b07c7635ac35c900f216dd8a8c0289948b4
Anand12082001 <160377737+Anand12082001@users.noreply.github.com>
2026-01-04 16:35:30 +0530

    Refactor user creation and email sending logic
    Refactor user addition functions to ensure database writes are awaited before sending emails. Update error handling and correct user creation logic.

--- controllers/Management/add-user.controller.js
+++ controllers/Management/add-user.controller.js
@@ -1,99 +1,235 @@
+// const Users = require('../../models/user.model');
+// // const bcrypt = require('bcrypt');
+// const bcrypt = require("bcrypt");
+
+// const sendMail = require("../../config/Nodemailer");
+// const emailTemplate = require("../../utlis/emailTemplates");
+// const generatePassword = require('../../utlis/generatePassword');
+
+// const AddTPO = async (req, res) => {
+//   const { email, first_name, number } = req.body;
+
+//   try {
+//     if (await Users.findOne({ email }))
+//       return res.json({ msg: "User Already Exists!" });
+
+//     const password = generatePassword();
+
+//     const hashPassword = await bcrypt.hash(password, 10);
+
+//     Users.create({ first_name, email, number, password: hashPassword, role: "tpo_admin" });
+
+//     const html = emailTemplate({
+//       role: "TPO",
+//       name: first_name,
+//       email: email,
+//       password: password
+//     });
+//     const subject = "Welcome to CPMS | Your Login Credentials as a TPO";
+
+//     await sendMail(email, subject, html);
+
+//     return res.json({ msg: "User Created!" });
+//   } catch (error) {
+//     console.log("management-user-add-tpo => ", error);
+//     return res.json({ msg: "Internal Server Error!" });
+//   }
+// }
+
+// const AddManagement = async (req, res) => {
+//   const { email, first_name, number } = req.body;
+
+//   try {
+//     if (await Users.findOne({ email }))
+//       return res.json({ msg: "User Already Exists!" });
+
+//     const password = generatePassword();
+
+//     const hashPassword = await bcrypt.hash(password, 10);
+
+//     Users.create({ first_name, email, number, password: hashPassword, role: "management_admin" });
+
+//     const html = emailTemplate({
+//       role: "Management",
+//       name: first_name,
+//       email: email,
+//       password: password
+//     });
+//     const subject = "Welcome to CPMS | Your Login Credentials as a Management";
+
+//     await sendMail(email, subject, html);
+//     return res.json({ msg: "User Created!" });
+//   } catch (error) {
+//     console.log("management-user-add-management => ", error);
+//     return res.json({ msg: "Internal Server Error!" });
+//   }
+// }
+
+// const AddStudent = async (req, res) => {
+//   const { email, first_name, number } = req.body;
+
+//   try {
+//     if (await Users.findOne({ email }))
+//       return res.json({ msg: "User Already Exists!" });
+
+//     const password = generatePassword();
+
+//     const hashPassword = await bcrypt.hash(password, 10);
+
+//     Users.create({ first_name, email, number, password: hashPassword, role: "student", "studentProfile.isApproved": true });
+
+//     const html = emailTemplate({
+//       role: "Student",
+//       name: first_name,
+//       email: email,
+//       password: password
+//     });
+//     const subject = "Welcome to CPMS | Your Login Credentials as a Student";
+
+//     await sendMail(email, subject, html);
+
+//     return res.json({ msg: "User Created!" });
+//   } catch (error) {
+//     console.log("management-user-add-management => ", error);
+//     return res.json({ msg: "Internal Server Error!" });
+//   }
+// }
+
+// module.exports = {
+//   AddTPO,
+//   AddManagement,
+//   AddStudent
+// };
 const Users = require('../../models/user.model');
-// const bcrypt = require('bcrypt');
 const bcrypt = require("bcrypt");
 
 const sendMail = require("../../config/Nodemailer");
-const emailTemplate = require("../../utlis/emailTemplates");
-const generatePassword = require('../../utlis/generatePassword');
+const emailTemplate = require("../../utils/emailTemplates");
+const generatePassword = require("../../utils/generatePassword");
 
+// ================= ADD TPO =================
 const AddTPO = async (req, res) => {
   const { email, first_name, number } = req.body;
 
   try {
-    if (await Users.findOne({ email }))
+    if (await Users.findOne({ email })) {
       return res.json({ msg: "User Already Exists!" });
+    }
 
     const password = generatePassword();
-
     const hashPassword = await bcrypt.hash(password, 10);
 
-    Users.create({ first_name, email, number, password: hashPassword, role: "tpo_admin" });
+    // 🔴 FIX 1: AWAIT USER CREATION
+    await Users.create({
+      first_name,
+      email,
+      number,
+      password: hashPassword,
+      role: "tpo_admin"
+    });
 
     const html = emailTemplate({
       role: "TPO",
       name: first_name,
-      email: email,
-      password: password
+      email,
+      password
     });
+
     const subject = "Welcome to CPMS | Your Login Credentials as a TPO";
 
+    // 🔴 FIX 2: EMAIL AFTER DB WRITE
     await sendMail(email, subject, html);
 
-    return res.json({ msg: "User Created!" });
-  } catch (error) {
-    console.log("management-user-add-tpo => ", error);
-    return res.json({ msg: "Internal Server Error!" });
-  }
-}
+    return res.json({ msg: "User Created & Email Sent!" });
 
+  } catch (error) {
+    console.log("add-tpo => ", error);
+    return res.status(500).json({ msg: "Internal Server Error!" });
+  }
+};
+
+// ================= ADD MANAGEMENT =================
 const AddManagement = async (req, res) => {
   const { email, first_name, number } = req.body;
 
   try {
-    if (await Users.findOne({ email }))
+    if (await Users.findOne({ email })) {
       return res.json({ msg: "User Already Exists!" });
+    }
 
     const password = generatePassword();
-
     const hashPassword = await bcrypt.hash(password, 10);
 
-    Users.create({ first_name, email, number, password: hashPassword, role: "management_admin" });
+    // 🔴 FIX 1
+    await Users.create({
+      first_name,
+      email,
+      number,
+      password: hashPassword,
+      role: "management_admin"
+    });
 
     const html = emailTemplate({
       role: "Management",
       name: first_name,
-      email: email,
-      password: password
+      email,
+      password
     });
+
     const subject = "Welcome to CPMS | Your Login Credentials as a Management";
 
+    // 🔴 FIX 2
     await sendMail(email, subject, html);
-    return res.json({ msg: "User Created!" });
-  } catch (error) {
-    console.log("management-user-add-management => ", error);
-    return res.json({ msg: "Internal Server Error!" });
-  }
-}
 
+    return res.json({ msg: "User Created & Email Sent!" });
+
+  } catch (error) {
+    console.log("add-management => ", error);
+    return res.status(500).json({ msg: "Internal Server Error!" });
+  }
+};
+
+// ================= ADD STUDENT =================
 const AddStudent = async (req, res) => {
   const { email, first_name, number } = req.body;
 
   try {
-    if (await Users.findOne({ email }))
+    if (await Users.findOne({ email })) {
       return res.json({ msg: "User Already Exists!" });
+    }
 
     const password = generatePassword();
-
     const hashPassword = await bcrypt.hash(password, 10);
 
-    Users.create({ first_name, email, number, password: hashPassword, role: "student", "studentProfile.isApproved": true });
+    // 🔴 FIX 1
+    await Users.create({
+      first_name,
+      email,
+      number,
+      password: hashPassword,
+      role: "student",
+      studentProfile: { isApproved: true }
+    });
 
     const html = emailTemplate({
       role: "Student",
       name: first_name,
-      email: email,
-      password: password
+      email,
+      password
     });
+
     const subject = "Welcome to CPMS | Your Login Credentials as a Student";
 
+    // 🔴 FIX 2
     await sendMail(email, subject, html);
 
-    return res.json({ msg: "User Created!" });
+    return res.json({ msg: "User Created & Email Sent!" });
+
   } catch (error) {
-    console.log("management-user-add-management => ", error);
-    return res.json({ msg: "Internal Server Error!" });
+    console.log("add-student => ", error);
+    return res.status(500).json({ msg: "Internal Server Error!" });
   }
-}
+};
 
 module.exports = {
   AddTPO,
